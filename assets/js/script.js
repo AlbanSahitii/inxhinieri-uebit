b62a55d5a1e509ee009cca457803469b2d53f163
Zgjim Grajçevci <grajqevcizgjim@gmail.com>
2022-12-06 22:53:21 +0100

    added assets

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+function openNav() {
+    document.getElementById("home_navbar_items_phone").style.width = "100%";
+}
+function closeNav() {
+    document.getElementById("home_navbar_items_phone").style.width = "0";
+}
