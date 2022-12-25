1c347677ec7bc1934697ad067ab1831914a2462c
Zgjim Grajçevci <grajqevcizgjim@gmail.com>
2022-12-25 18:22:54 +0100

    new files

---
+++ assets/js/login.js
@@ -0,0 +1,59 @@
+
+// UI variables
+const form = document.querySelector('#form')
+    email = document.querySelector('#email'),
+    password = document.querySelector('#password');
+// listen for submit
+form.addEventListener('submit', (e) => {
+    // prebent form from submitting
+    e.preventDefault()
+
+    // get input values
+    const emailValue = email.value.trim(),
+        passwordValue = password.value.trim();
+
+    if(emailValue === '') {
+        showError(email, 'Email cannot be empty');
+    }else if(!emailValidate(emailValue)) {
+        showError(email, 'Enter a correct email');
+    }
+    else {
+        showSuccess(email)
+    }
+
+    if(passwordValue === '') {
+        showError(password, 'Password cannot be empty');
+    }else {
+        showSuccess(password);
+    }
+
+});
+
+//  error
+function showError(input, message) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-group error';
+    formControl.querySelector('.small').innerText = message
+
+}
+
+// display error
+function displayError(message) {
+    const div = document.createElement('div');
+    div.className = 'alert'
+    div.innerText = message;
+    const container = document.querySelector('.container');
+    container.insertBefore(div, form)
+}
+
+// success
+function showSuccess(input) {
+    formControl = input.parentElement;
+    formControl.className = 'form-group success';
+}
+
+// email Validation
+function emailValidate(emailVal) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(emailVal).toLowerCase());
+}
