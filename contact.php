a571092498c3641d03bcd1887dd296d3963ad0fd
AlbanSahitii <109045631+AlbanSahitii@users.noreply.github.com>
2023-02-16 15:29:35 +0100

    Better contact us form

---
+++ contact.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="assets/css/home.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap"
+      rel="stylesheet"
+    />
+    <script src="assets/js/watchlist.js" defer></script>
+    <title>Contact Us</title>
+  </head>
+  <body>
+    <div class="container">
+        <?php
+          include("templates/navbar.php");
+        ?>
+
+        <div class="right-container">
+            <div class="contact-form">
+                <form>
+                    <input type="text" id="fname" name="firstname" placeholder="Your name.." required>
+                    <input type="text" id="lname" name="lastname" placeholder="Your last name.." required>          
+                    <textarea id="subject" name="subject" placeholder="Write something.." required></textarea>
+                    <input type="submit" value="Submit">
+                  </form>
+            </div>
+        </div>
+    </div>
+   </div>
+  </body>
+</html>
