9475311b3d5314aefe9c5fc2284f38dd19edb374
AlbanSahitii <109045631+AlbanSahitii@users.noreply.github.com>
2023-02-22 22:32:16 +0100

    ;

--- assets/js/explore.js
+++ assets/js/explore.js
@@ -17,27 +17,12 @@ const addToWatchList = function (movieId) {
       success: function (data, status) {
         console.log(status);
       },
-      error: function (jqXHR, exception) {
-        var msg = "";
-        if (jqXHR.status === 0) {
-          msg = "Not connect.\n Verify Network.";
-        } else if (jqXHR.status == 404) {
-          msg = "Requested page not found. [404]";
-        } else if (jqXHR.status == 500) {
-          msg = "Internal Server Error [500].";
-        } else if (exception === "parsererror") {
-          msg = "Requested JSON parse failed.";
-        } else if (exception === "timeout") {
-          msg = "Time out error.";
-        } else if (exception === "abort") {
-          msg = "Ajax request aborted.";
-        } else {
-          msg = "Uncaught Error.\n" + jqXHR.responseText;
-        }
-        console.log(msg);
+      error: function (err) {
+        console.log(err);
       },
     });
   });
+  window.location.reload();
 };
 
 const insertMovies = function (data, status, settings) {
